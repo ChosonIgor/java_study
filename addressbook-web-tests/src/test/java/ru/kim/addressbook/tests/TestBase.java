7f37ace42d5bc662bc0e043849e23e77a8c3872b
ChosonIgor <51879005+ChosonIgor@users.noreply.github.com>
2021-06-20 19:25:33 +0300

    Добавлена проверка списков групп до и после создания/удыления/модификации группы

--- addressbook-web-tests/src/test/java/ru/kim/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/kim/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.kim.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod()
     public void setUp() {

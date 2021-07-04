d6bae512459612405e6d718e6684cbfe4dc6c523
ChosonIgor <51879005+ChosonIgor@users.noreply.github.com>
2021-07-04 19:09:42 +0300

    Добавлен класс формирования тестовых данных в виде файла json для контактов

--- addressbook-web-tests/src/test/java/ru/kim/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/kim/addressbook/model/ContactData.java
@@ -1,23 +1,38 @@
 package ru.kim.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 import java.util.Objects;
 
 public class ContactData {
     private int id  = Integer.MAX_VALUE;
     private String firstName;
+    @Expose
     private String middleName;
+    @Expose
     private String lastName;
+    @Expose
     private String nickName;
+    @Expose
     private String company;
+    @Expose
     private String address;
+    @Expose
     private String email;
+    @Expose
     private String email2;
+    @Expose
     private String email3;
+    @Expose
     private String allEmail;
+    @Expose
     private String homePhone;
+    @Expose
     private String mobilePhone;
+    @Expose
     private String workPhone;
+    @Expose
     private String allPhones;
     private File photo;
 

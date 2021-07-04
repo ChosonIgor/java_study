6896f7181a13cb290fb0c496b2726d2e930939f5
ChosonIgor <51879005+ChosonIgor@users.noreply.github.com>
2021-07-05 00:47:44 +0300

    Добавил описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/kim/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/kim/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    Properties properties;
     protected WebDriver wd;
     private ContactHelper homeHelper;
     private NavigationHelper navigationHelper;
@@ -17,21 +21,24 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
         }
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.BaseUrl"));
         homeHelper = new ContactHelper(wd);
         SessionHelper sessionHelper = new SessionHelper(wd);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {

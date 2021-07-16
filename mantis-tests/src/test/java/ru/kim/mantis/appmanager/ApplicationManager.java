02270636f8fe6007c89452e1a103e4c885198268
ChosonIgor <51879005+ChosonIgor@users.noreply.github.com>
2021-07-16 23:30:22 +0300

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/kim/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,46 @@
+package ru.kim.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+    Properties properties;
+    protected WebDriver wd;
+
+    private String browser;
+
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.BaseUrl"));
+
+
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+
+}
